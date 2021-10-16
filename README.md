8b6c7e6d60a6d5c9762be7a569b8f86b50dd1fba
Menci <huanghaorui301@gmail.com>
2021-10-16 21:59:18 +0800

    Initial version

---
+++ README.md
@@ -0,0 +1,43 @@
+# GitHub Action for Deploying SSL certificate to Upyun
+
+Deploy SSL certificate to Upyun CDN or OSS.
+
+# Usage
+
+> If you need to issue SSL certificates automatically, you can use [my acme.sh action](https://github.com/marketplace/actions/issue-ssl-certificate).
+
+This action will deploy your PEM-formatted SSL certificate to Upyun. Since it uses Upyun's console API, you must use a subaccount (NOT your main account since it requires 2FA) as credential.
+
+```yaml
+jobs:
+  deploy-to-upyun:
+    name: Deploy certificate to Upyun
+    runs-on: ubuntu-latest
+    steps:
+      - name: Check out
+        uses: actions/checkout@v2
+        with:
+          # If you just commited and pushed your newly issued certificate to this repo in a previous job,
+          # use `ref` to make sure checking out the newest commit in this job
+          ref: ${{ github.ref }}
+      - uses: Menci/deploy-certificate-to-aliyun@beta-v1
+        with:
+          # Subaccount
+          subaccount-username: ${{ secrets.UPYUN_SUBACCOUNT_USERNAME }}
+          subaccount-password: ${{ secrets.UPYUN_SUBACCOUNT_PASSWORD }}
+
+          # Specify PEM fullchain file
+          fullchain-file: ${{ env.FILE_FULLCHAIN }}
+          # Specify PEM private key file
+          key-file: ${{ env.FILE_KEY }}
+
+          # Deploy to CDN or OSS
+          cdn-domains: |
+            cdn1.example.com
+            cdn2.example.com
+            oss1.example.com
+            oss2.example.com
+          
+          # Delete ALL unused certificates after deployment
+          delete-unused-certificates: true
+```
---
+++ index.ts
@@ -0,0 +1,153 @@
+import fs from "fs";
+import { URL } from "url";
+import * as core from "@actions/core";
+import Axios, { Method } from "axios";
+import { wrapper } from "axios-cookiejar-support";
+import { CookieJar } from "tough-cookie";
+
+const jar = new CookieJar();
+const client = wrapper(Axios.create({ jar }));
+
+const input = {
+  subaccountUsername: core.getInput("subaccount-username"),
+  subaccountPassword: core.getInput("subaccount-password"),
+  fullchainFile: core.getInput("fullchain-file"),
+  keyFile: core.getInput("key-file"),
+  domains: core.getInput("domains"),
+  deleteUnusedCertificates: core.getBooleanInput("delete-unused-certificates")
+};
+
+async function callApi<ResponseData>(url: string, data: Record<string, unknown>, method: Method = "POST") {
+  interface UpyunConsoleApiResponse {
+    data: ResponseData;
+    msg: {
+      errors: string[];
+      messages: string[];
+    }
+    user: unknown;
+  }
+
+  const result = await client.request({
+    method,
+    url,
+    data
+  });
+  const response = result.data as UpyunConsoleApiResponse;
+  console.log(`${url}: ${JSON.stringify(response.msg)}`);
+
+  return response.data;
+}
+
+async function login() {
+  const responseData = await callApi<{ result: boolean }>(
+    "https://console.upyun.com/accounts/signin/",
+    {
+      username: input.subaccountUsername,
+      password: input.subaccountPassword
+    }
+  );
+
+  if (!responseData.result) {
+    throw new Error(`Failed to login: ${JSON.stringify(responseData)}`);
+  }
+}
+
+async function uploadCertificate() {
+  const fullchain = fs.readFileSync(input.fullchainFile, "utf-8");
+  const key = fs.readFileSync(input.keyFile, "utf-8");
+
+  const responseData = await callApi<{ status: number; result: { certificate_id: string } }>(
+    "https://console.upyun.com/api/https/certificate/",
+    {
+      certificate: fullchain,
+      private_key: key
+    }
+  );
+
+  if (responseData.status !== 0) {
+    throw new Error(`Failed to upload certificate: ${JSON.stringify(responseData)}`);
+  }
+
+  return responseData.result.certificate_id;
+}
+
+async function deployCertificate(id: string) {
+  const domains = Array.from(new Set(input.domains.split(/\s+/).filter(x => x)));
+  
+  for (const domain of domains) {
+    console.log(`Deploying certificate to domain ${domain}.`);
+
+    const responseData = await callApi<{ result: boolean }>(
+      "https://console.upyun.com/api/https/migrate/domain",
+      {
+        crt_id: id,
+        domain_name: domain
+      }
+    );
+
+    if (!responseData.result) {
+      throw new Error(`Failed to deploy certificate to domain "${domain}": ${JSON.stringify(responseData)}`);
+    }
+  }
+}
+
+async function deleteUnusedCertificates() {
+  async function listUnusedCertificates(callback: (id: string) => Promise<void>) {
+    let pager = {
+      since: null as number,
+      max: null as number,
+      limit: 10
+    };
+
+    while (true) {
+      const url = new URL("https://console.upyun.com/api/https/certificate/list/");
+      for (const key in pager) if (pager[key] != null) url.searchParams.set(key, String(pager[key]));
+
+      const responseData = await callApi<{ result: Record<string, { config_domain: number }>; pager: typeof pager; status: number }>(
+        url.toString(),
+        undefined,
+        "GET"
+      );
+
+      if (responseData.status !== 0) {
+        throw new Error(`Failed to list certificates: ${JSON.stringify(responseData)}`);
+      }
+
+      for (const id in responseData.result) {
+        if (id !== "default" && responseData.result[id].config_domain === 0)
+          await callback(id);
+      }
+
+      pager = responseData.pager;
+      if (pager.max == null) break;
+    }
+  }
+
+  await listUnusedCertificates(async id => {
+    const responseData = await callApi<{ status: boolean }>(
+      `https://console.upyun.com/api/https/certificate/?certificate_id=${id}`,
+      undefined,
+      "DELETE"
+    );
+
+    if (responseData.status !== true) {
+      throw new Error(`Failed to delete unused certificate ${id}: ${JSON.stringify(responseData)}`);
+    }
+  });
+}
+
+async function main() {
+  await login();
+
+  const id = await uploadCertificate();
+
+  if (input.domains) await deployCertificate(id);
+
+  if (input.deleteUnusedCertificates) await deleteUnusedCertificates();
+}
+
+main().catch(error => {
+  console.log(error.stack);
+  core.setFailed(error);
+  process.exit(1);
+});
